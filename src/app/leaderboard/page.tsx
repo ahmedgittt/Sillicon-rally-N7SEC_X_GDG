754b0fbc60d01f930052447d64c76585303dbd98
ahmedgittt <ahmed.rajji05@gmail.com>
2025-12-08 21:55:50 +0100

    test pull

--- src/app/leaderboard/page.tsx
+++ src/app/leaderboard/page.tsx
@@ -4,7 +4,7 @@ export default function Page() {
   return (
     <div className="flex flex-col items-center justify-center gap-20">
       <h1 className="text-4xl md:text-6xl font-black tracking-tighter bg-clip-text text-transparent bg-gradient-to-r from-primary via-accent to-primary mt-20">
-        Global Leaderboard
+        Global Leaderboard f
       </h1>
       <Leaderboard />
     </div>
